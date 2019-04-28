c28aca559ada5aae1c3a05151c6b4c1b20da5322
furixturi <alabebop@gmail.com>
2019-04-28 13:00:06 +0900

    Initial Commit

---
+++ RockPaperScissorsSwift WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RockPaperScissorsSwift WatchKit Extension
+//
+//  Created by Xiaoli Shen on 2019/04/28.
+//  Copyright © 2019 furixturi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ RockPaperScissorsSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissorsSwift
+//
+//  Created by Xiaoli Shen on 2019/04/28.
+//  Copyright © 2019 furixturi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
