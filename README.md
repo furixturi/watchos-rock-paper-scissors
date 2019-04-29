49b4b2dba145dafc7b886652b2cef716c9ff7bba
furixturi <alabebop@gmail.com>
2019-04-29 12:27:49 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Got an apple watch.
+Played around with it.
+Use XCode to further play around with it.
+
+And here is my first watchOS app!
+
+Thanks to [this
+tutorial](https://dev.to/andreslopezrm/its-time-to-apple-watchcreate-a-game-with-watchos-swift--2660)
+
--- RockPaperScissorsSwift WatchKit Extension/InterfaceController.swift
+++ RockPaperScissorsSwift WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,87 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var opponentLabel: WKInterfaceLabel!
+    @IBOutlet weak var resultLabel: WKInterfaceLabel!
+    @IBOutlet weak var counterLabel: WKInterfaceLabel!
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    
+    @IBOutlet weak var rockButton: WKInterfaceButton!
+    @IBOutlet weak var paperButton: WKInterfaceButton!
+    @IBOutlet weak var scissorsButton: WKInterfaceButton!
+    
+    enum Option: String {
+        case rock
+        case paper
+        case scissors
+        
+        var image: UIImage? {
+            return UIImage(named: rawValue)
+        }
+    }
+    
+    enum Result: String {
+        case win = "WIN"
+        case tie = "TIE"
+        case lose = "LOSE"
+        
+        func update(label: WKInterfaceLabel) {
+            label.setText(rawValue)
+        }
+    }
+    
+    let options: [Option] = [.rock, .paper, .scissors]
+    var counter = 0
+    
+    func check(_ selection: Option) {
+        let randomOptions = options.shuffled()
+        let machineChoose = randomOptions[0]
+        let result: Result
+        
+        switch (selection, machineChoose) {
+        case (.rock, .scissors),
+             (.paper, .rock),
+             (.scissors, .paper):
+             result = .win
+        case (.rock, .paper),
+             (.paper, .scissors),
+            (.scissors, .rock):
+            result = .lose
+        default:
+            result = .tie
+        }
+        
+        if result == .win {
+            counter += 1
+        }
+        
+        result.update(label: resultLabel)
+        counterLabel.setText("Wins: \(counter)")
+        opponentLabel.setText("Opponent: \(machineChoose)")
+    }
+    
+
+    
+    @IBAction func rock() {
+        check(.rock)
+    }
+    
+    @IBAction func paper() {
+        check(.paper)
+    }
+    
+    @IBAction func scissors() {
+        check(.scissors)
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        rockButton.setBackgroundImage(Option.rock.image)
+        paperButton.setBackgroundImage(Option.paper.image)
+        scissorsButton.setBackgroundImage(Option.scissors.image)
+        
+        timer.start()
     }
     
     override func willActivate() {
